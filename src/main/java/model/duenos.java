1d06a8e6315ed1c99d672ea23e944bdaf3330931
Osunita <raulosunaabadias71@gmail.com>
2023-03-22 12:25:11 +0100

    a3

--- src/main/java/model/duenos.java
+++ src/main/java/model/duenos.java
@@ -13,9 +13,6 @@ public class duenos {
 	private String dni;
 	
 	
-	public duenos(String string, String string2) {
-		// TODO Auto-generated constructor stub
-	}
 	public String getNombreDueño() {
 		return nombreDueño;
 	}
@@ -29,33 +26,5 @@ public class duenos {
 		this.dni = dni;
 	}
 
-	
-	public List<duenos> getOwners() {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        List<duenos> owners = new ArrayList<>();
-
-        try {
-            conn = getConnection();
-            String sql = "SELECT nombre, dni FROM owners";
-            stmt = conn.prepareStatement(sql);
-            rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                duenos owner = new duenos(rs.getString("nombre"), rs.getString("dni"));
-                owners.add(owner);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            closeResultSet(rs);
-            closeStatement(stmt);
-            closeConnection(conn);
-        }
-
-        return owners;
-    }
 
 }
